8d2dfb6e03d63ee2e2da2adcf0e1ded48ba08160
Danielle Gibson <peyton.gibson@stu.barren.kyschools.us>
2024-12-05 08:08:10 -0600

    Initial Commit

---
+++ Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Student on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
